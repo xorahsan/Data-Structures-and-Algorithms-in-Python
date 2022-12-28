78b331559d92a372c77a969baec4655bfabdc0b9
Ahsan <xorahsan@gmail.com>
2022-12-28 17:23:54 +0500

    🔥 Doubly Linked List

--- DATA_STRUCTURES/LINEAR/LINKED_LISTS/doubly_linked_lists.py
+++ DATA_STRUCTURES/LINEAR/LINKED_LISTS/doubly_linked_lists.py
@@ -1,6 +1,5 @@
-
 """
-======================= Singly Linked List ======================
+======================= Doubly Linked List ======================
 
 Traversal Time Complexity: O(n)
 Insertion/Deletion Time Complexity at start: O(1)
@@ -9,17 +8,21 @@ Deletion Time Complexity: O(n)
 
 Node class Variables:
     value: value of current node
-    next: whole next Node
-
-Singly Linked List class variables:
-    head: the whole node of current singly linked list head
+    next: whole next node
+    prev: whole previous node
 
 
-Singly Linked List Methods:
+Doubly Linked List class variables:
+    head: the whole node of current doubly linked list head
+
+
+Doubly Linked List Methods:
 
 insertAtBegin(data) # insert Node at start
 
 printAll() # print every element of current linked list
+
+print(reverse=True) #print doubly linked list in reverse order
 
 length() # return length of current Linked List
 
@@ -34,22 +37,25 @@ find(data) #return index of first occurence of given value
 """
 
 class Node:
-    def __init__(self, data = None, next = None):
+    def __init__(self, data = None, next = None, prev=None):
         self.data = data
         self.next = next
+        self.prev = prev
 
 class SinglyLinkedList:
     def __init__(self):
         self.head = None
     
     def insertAtBegin(self, item):
-        self.head = Node(item, self.head)
+        temp = Node(item, self.head)
+        if self.head: self.head.prev = temp
+        self.head = temp
 
     def insertAtEnd(self, item):
         if not self.head:  return self.insertAtBegin(item)
         i = self.head
         while i.next: i = i.next
-        i.next = Node(item)
+        i.next = Node(item,None,i)
         
     def length(self):
         i, j = 0,self.head
@@ -59,11 +65,13 @@ class SinglyLinkedList:
     def removeAt(self, ind):
         if ind < 0 or ind >= self.length(): return print("INVALID Index")
         if ind == 0:
+            self.head.next.prev = None
             self.head = self.head.next
             return
         i,j = 0,self.head
         while i < ind-1: i,j = i+1,j.next
         j.next = None if i == self.length()-2 else j.next.next
+        j.next.prev = j
 
     def insertAt(self, ind, item):
         if ind < 0 or ind > self.length(): return print("INVALID Index")
@@ -72,29 +80,47 @@ class SinglyLinkedList:
 
         i,j = 0,self.head
         while i < ind-1: i,j = i+1,j.next
-        j.next = Node(item) if not j.next else Node(item,j.next)
+        j.next = Node(item,None,j) if not j.next else Node(item,j.next,j)
 
-    def print(self):
+    def find(self, item):
+        i,j = 0,self.head
+        while i < self.length():
+            if j.data == item: return i
+            i,j=i+1,j.next
+        return -1
+
+
+    def print(self, reverse = False):
+        
         if not self.head: return print("Linked List is empty")
         i = self.head
-        while i:
-            print(i.data, end=" => ")
+        while i.next:
+            if not reverse: print(i.data, end=" => ")
             i = i.next
-        print()
+        if not reverse: return print(i.data)
+        while i.prev:
+            print(i.data, end=" => ")
+            i = i.prev
+        print(i.data)
+
         
 def main():
     sll = SinglyLinkedList()
     sll.insertAtEnd(6)
     sll.print()
+    sll.print(reverse=True)
     print(sll.length())
     sll.insertAtBegin(6)
     sll.print()
+    sll.print(reverse=True)
     sll.removeAt(0)
+    sll.print(reverse=True)
     sll.print()
     sll.insertAtEnd(5)
     print(sll.length())
     sll.insertAtBegin(4)
     sll.print()
+    sll.print(reverse=True)
     print(sll.length())
     sll.insertAtBegin(3)
     sll.print()
@@ -107,11 +133,15 @@ def main():
     print(sll.length())
     sll.insertAtEnd(9)  
     sll.print()
+    sll.print(reverse=True)
     print(sll.length())
     sll.removeAt(0)
     sll.print()
     sll.insertAt(6, 0)
     sll.print()
+    sll.print(reverse=True)
+    print(sll.find(9))
+
 
 if __name__ == "__main__":
     main()
